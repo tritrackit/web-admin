6080f4d95583bd10bcca24e32a0030c9cd27cb12
tritrackit <tritrackit@gmail.com>
2025-08-10 05:43:01 +0800

    updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: "https://tritrackit-api.vercel.app/api",
+  apiBaseUrl: "https://tritrackit-api.vercel.app/api/v1",
   idleTimeoutMinutes: 30,
   pusher: {
     key: "f1456b01b6514f09b9a3",
